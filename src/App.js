3c3acdae849629b2852badcf70c8b8f82e696ea4
Amirhossein2001 <amirhosseinramezani4455@gmail.com>
2025-05-23 23:42:53 +0330

    Inline-style conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 
 function App() {
-  const age = 2;
+  const age = 24;
 
   return (
     <div className="App">
       {age > 20 ? <h1>Over</h1> : <h1>Under</h1>}
       {age == 27 && <h1>Age is 20</h1>}
+
+      <button type="submit" className= {age > 20 ? "success" : "warning"}>Click Me</button>
     </div>
   );
 }
