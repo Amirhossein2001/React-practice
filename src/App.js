5c3b6a2afc54da55e3d297aa957d7a5452e2dd72
Amirhossein2001 <amirhosseinramezani4455@gmail.com>
2025-05-23 17:32:35 +0330

    props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const name = "Amir"
+  const name = "Amir";
   return (
     <div className="App">
-       <div className=""><h1>{name}</h1></div>
+      <div className="">
+        <h1>{name}</h1>
+        <GetUser name="Amir" age={23} email="Amir@gmail.com" />
+      </div>
     </div>
   );
 }
+
+const GetUser = (props) => {
+  return (
+    <div className="">
+      <div className="">{props.name}</div>
+      <div className="">{props.age}</div>
+      <div className="">{props.email}</div>
+    </div>
+  );
+};
 
 export default App;
